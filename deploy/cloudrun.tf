69d746ce379cf59a4acd1d49425bb78ae0188fca
Russell Hancox <russellhancox@users.noreply.github.com>
2025-02-04 15:30:50 -0500

    Fix IAM bindings (#16)

--- deploy/cloudrun.tf
+++ deploy/cloudrun.tf
@@ -27,6 +27,30 @@ resource "google_cloud_run_v2_service" "default" {
       }
     }
   }
+
+  lifecycle {
+    # Images are updated by the CI/CD pipeline.
+    ignore_changes = [
+      template.0.containers.0.image,
+      template.0.labels,
+    ]
+  }
+}
+
+resource "google_cloud_run_v2_service_iam_member" "public_access" {
+  project  = google_cloud_run_v2_service.default.project
+  location = google_cloud_run_v2_service.default.location
+  name     = google_cloud_run_v2_service.default.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
+
+resource "google_cloud_run_v2_service_iam_member" "federated_run_admin" {
+  project  = google_cloud_run_v2_service.default.project
+  location = google_cloud_run_v2_service.default.location
+  name     = google_cloud_run_v2_service.default.name
+  role     = "roles/run.admin"
+  member   = "principal://iam.googleapis.com/projects/${data.google_project.default.number}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.default.workload_identity_pool_id}/subject/repo:${var.github_repo}:ref:refs/heads/main"
 }
 
 resource "google_cloud_run_domain_mapping" "default" {
